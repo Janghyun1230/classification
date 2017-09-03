8965d712dd25ce6b9965d2b9a54edb1c5d778be8
JangHyun <blue_378@naver.com>
2017-09-03 20:40:12 +0900

    Add files via upload

--- network.py
+++ network.py
@@ -25,11 +25,17 @@ def tanh(x):
 def network(name, x, layers, is_training = True):
     tensors = {}
     with tf.variable_scope(name):
+        tensors['input'] = x
         for (layer_name, prop) in layers:
             with tf.variable_scope(layer_name):
                 shape = prop.get('reshape', None)
+                input = prop.get('input', None)
                 pad = prop.get('pad', 'SAME')
                 batch = prop.get('batch', None)
+                concat = prop.get('concat', None)
+                add = prop.get('add', None)
+                if input is not None:
+                    x = tensors[input]
                 if shape is not None:
                     x = tf.reshape(x, x.get_shape().as_list()[:1] + shape)
                 if layer_name[:2] == 'fc':
@@ -39,10 +45,8 @@ def network(name, x, layers, is_training = True):
                                                               normalizer_params={'decay': 0.9,   # this works better than 0.999
                                                               'scale': True, 'is_training': is_training}
                                                               )
-                        tensors[layer_name] = x
                     else:
                         x = tf.contrib.layers.fully_connected(x, num_outputs=prop['n_out'], activation_fn=prop['act'])
-                        tensors[layer_name] = x
                 elif layer_name[:4] == 'conv':
                     if batch is not None:
                         x = tf.contrib.layers.conv2d(inputs=x, num_outputs=prop['n_out'], kernel_size=prop['k'],
@@ -51,12 +55,10 @@ def network(name, x, layers, is_training = True):
                                                      normalizer_params={'decay': 0.9,
                                                                         'scale': True, 'is_training': is_training}
                                                      )
-                        tensors[layer_name] = x
                     else:
                         x = tf.contrib.layers.conv2d(inputs=x, num_outputs=prop['n_out'], kernel_size=prop['k'],
                                                      stride=prop['s'], padding=pad, activation_fn=prop['act']
                                                      )
-                        tensors[layer_name] = x
                 elif layer_name[:4] == 'cnvT':
                     if batch is not None:
                         x = tf.contrib.layers.conv2d_transpose(inputs=x, num_outputs=prop['n_out'], kernel_size=prop['k'],
@@ -65,13 +67,16 @@ def network(name, x, layers, is_training = True):
                                                      normalizer_params={'decay': 0.9,
                                                                         'scale': True, 'is_training': is_training}
                                                      )
-                        tensors[layer_name] = x
                     else:
                         x = tf.contrib.layers.conv2d_transpose(inputs=x, num_outputs=prop['n_out'], kernel_size=prop['k'],
                                                      stride=prop['s'], padding=pad, activation_fn=prop['act']
                                                      )
-                        tensors[layer_name] = x
-                        
+                tensors[layer_name] = x
+                if concat is not None:
+                    x = tf.concat([x, tensors[prop['concat']]], axis=(len(x.shape)-1))
+                if add is not None:
+                    x = x+tensors[prop['add']]
+
     vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, name)
     moving_avgs = tf.get_collection(tf.GraphKeys.UPDATE_OPS,name)  # these are assigin nodes of moving_average -non trainable variables
     
@@ -81,44 +86,37 @@ def network(name, x, layers, is_training = True):
 if __name__ == "__main__":
     print("start checking networks")
 
-    layers_d = [('conv1', {'n_out': 64, 'k': 4, 's': 2, 'act': lrelu}),
-                ('conv2', {'n_out': 128, 'k': 4, 's': 2, 'act': lrelu, 'batch': True}),
-                ('fc3', {'n_out': 1024, 'act': lrelu, 'batch': True, 'reshape': [-1]}),
-                ('fc4', {'n_out': 1, 'act': sigmoid})
+    layers = [('fc1', {'n_out': 100, 'act': None}),                                                    # Nx100
+                ('fc2', {'n_out': 16*16*3, 'act': relu, 'batch': True}),                              # Nx16x16x3
+                ('conv3', {'reshape': [16, 16, 3], 'n_out': 64, 'k': 4, 's': 2, 'act': lrelu}),      # Nx8x8x64
+                ('conv4', {'n_out': 64, 'k': 4, 's': 2, 'act': elu, 'batch': True}),                 # Nx4x4x64
+                ('cnvT5', {'n_out': 64, 'k': 4, 's': 2, 'act': elu, 'concat': 'conv3'}),            # Nx8x8x(64+64)
+                ('cnvT6', {'n_out': 3, 'k': 4, 's': 2, 'act': lrelu, 'batch': True}),                # Nx16x16x3
+                ('fc7', {'reshape': [-1], 'n_out': 100, 'act': relu, 'batch': True, 'add': 'fc1'}), # Nx100
+                ('fc8', {'n_out': 1, 'act': sigmoid})                                                  # Nx1
                 ]
 
-    layers_g = [('fc1', {'n_out': 1024, 'act': relu, 'batch': True}),
-                ('fc2', {'n_out': 7 * 7 * 128, 'act': relu, 'batch': True}),
-                ('cnvT3', {'n_out': 64, 'k': 4, 's': 2, 'act': relu, 'batch': True, 'reshape': [7, 7, 128]}),
-                ('cnvT4', {'n_out': 1, 'k': 4, 's': 2, 'act': tanh})
-                ]
 
-    print("variables")
-
-    a = tf.placeholder(shape = [64,28,28,1], dtype = tf.float32)
     z = tf.placeholder(shape = [64,64], dtype = tf.float32)
-
-    x, layers_d, d_vars, _ = network('discriminator', a, layers_d)
-    x_g, layers_g, g_vars, _ = network('generator', z, layers_g)
+    x, layers_d, d_vars, d_moving_avg = network('discriminator', z, layers)
 
     n = 0
-    print("discriminator output:", x.shape)
-    print("layers")
+    print("\n discriminator output:", x.shape)
+    print("\n layers")
     for i ,j in layers_d.items():
         print(i ,j)
         n += np.prod(j.get_shape().as_list())
-    print()
-    print("generator output:", x_g)
-    print("layers")
-    for i ,j in layers_g.items():
-        print(i ,j)
-        n += np.prod(j.get_shape().as_list())
+
+    print("\n moving average assign nodes")
+    print(d_moving_avg)
+
     print("\n total data size: ", n)
 
     n = 0
     for v in d_vars:
         n += np.prod(v.get_shape().as_list())
-    for v in g_vars:
-        n += np.prod(v.get_shape().as_list())
-    print("\n total variable size: ", n)
+    print(" total variable size: ", n)
+
+    # with tf.Session() as sess:
+    #     writer = tf. summary.FileWriter(r"C:\Users\LG\Documents\ex", sess.graph)
 
